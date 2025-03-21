8bc07c8569bb62a76f7b29f62b2c945257b3bf4f
Serge Fumey <sergefumey@MacBook-Pro-de-Serge.local>
2025-03-21 22:06:21 +0100

    Fix diagnostics.tf to use count instead of for_each

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -1,8 +1,11 @@
-output "vm_ids" {
-  value = concat(
-    azurerm_windows_virtual_machine.windows_vm[*].id,
-    azurerm_linux_virtual_machine.rhel_vm[*].id,
-    [azurerm_windows_virtual_machine.jumpbox.id]
-  )
+output "jumpbox_vm_id" {
+  value = azurerm_windows_virtual_machine.jumpbox.id
 }
 
+output "windows_vm_ids" {
+  value = azurerm_windows_virtual_machine.windows_vm[*].id
+}
+
+output "redhat_vm_ids" {
+  value = azurerm_linux_virtual_machine.rhel_vm[*].id
+}
