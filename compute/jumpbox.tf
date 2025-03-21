effdfbd6581337622e664a1c6014cab5e233eb52
Serge Fumey <sergefumey@MacBook-Pro-de-Serge.local>
2025-03-21 19:38:14 +0100

    Update variables

--- compute/jumpbox.tf
+++ compute/jumpbox.tf
@@ -1,3 +1,27 @@
+# Public IP for Jumpbox
+resource "azurerm_public_ip" "jumpbox_pip" {
+  name                = "jumpbox-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface for Jumpbox
+resource "azurerm_network_interface" "jumpbox_nic" {
+  name                = "jumpbox-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jumpbox_pip.id
+  }
+}
+
+# Jumpbox Virtual Machine
 resource "azurerm_windows_virtual_machine" "jumpbox" {
   name                  = "win11-jumpbox"
   location              = var.location
@@ -20,6 +44,7 @@ resource "azurerm_windows_virtual_machine" "jumpbox" {
   }
 }
 
+# Output Public IP of Jumpbox
 output "jumpbox_public_ip" {
   value = azurerm_public_ip.jumpbox_pip.ip_address
 }
